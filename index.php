26bfa4517120b9564832cb75a8222f91c50c8606
Shakilvai7203 <98231736+Shakilvai7203@users.noreply.github.com>
2022-02-03 08:27:15 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+	<html >
+	<head>
+		<title>Mr. Sulfuric</title>
+		<style> body{text-align: center; background-color: black;}img{text-align: center;}h1{color: red;text-align: center;font-family: Stenci;}h3{color: white;text-align: center;}h4{color: grey;text-align: center;}h5{color: white;text-align: center;}#kotak{width: 100%;height: 30px;background-color: red;}#text{padding-top: 5px;color: black;}</style>
+	</head>
+	<body><br><br>
+	<img src='https://media.giphy.com/media/MJttD4oi3Hg4M/giphy.gif'>
+	<h1><b> Hacked by BD Muslim hacker ></b></h1><br>
+	<h3>Don't play with me.</h3>
+	<h4>I am sulfuric</h4>
+	<h5>I'll burn it.</h5><br><br>
+	
+	
+	
+	
+	
+	
+	
+	<div id='kotak'>
+	<b><marquee id='text'>[Our prophet Hazrat Muhammad (SAW)]</marquee></div></b>
+	</body>
+	</html>
